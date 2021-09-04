e8961648dfdcff54ad3db05d70a9c8a2489ed0b2
unknown <bcalabrese04@gmail.com>
2021-09-04 15:59:33 -0300

    Tateti Version 2

--- Laboratorio/ejercicios/Tateti_v2/Tateti.java
+++ Laboratorio/ejercicios/Tateti_v2/Tateti.java
@@ -1,31 +1,23 @@
+import javax.smartcardio.ATR;
 import javax.swing.*;
-
-import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.HashSet;
 import java.util.ArrayList;
 
-public class Main {
-    static Boolean j = true;
-    static int winner = 0;
+public class Tateti extends JFrame {
+    private HashSet<JButton> botones;
+    public static int winner = 0;
+    public static Boolean j = true;
 
+    public Tateti() {
+        this.botones = new HashSet<>();
+    }
 
-/*
-c=0
-0 - 0 - X - X - X - X - 0 - X - 0
-0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8
-
-| O | O | X |
-| X | X | X |
-| O | X | O |
-*/
-
-
-    public static int ver_ganador(JPanel tab) {
+    public int ver_ganador(JPanel tab) {
         //Crear matriz
         int ganador = 0;
 
@@ -71,9 +63,27 @@ c=0
         return ganador;
     }
 
-    public static void main(String[] args) {
-        
+    public void anotar(JButton b1){
+        String t = "";
+        if(j){
+            t = "X";
+        }
+        else{
+            t = "O";
+        }
+        if(b1.getText() == ""){
+            b1.setText(t);
+            if(j){
+                j = false;
+            }
+            else{
+                j = true;
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        Tateti t1 = new Tateti();
         JFrame ventana = new JFrame();
         ventana.setLayout(null);
         ventana.setSize(500, 500);
@@ -129,6 +139,11 @@ c=0
         JButton b9 = new JButton();
         tablero.add(b9);
 
+        for (int i = 0; i < tablero.getComponents().length; i++) {
+            JButton b = (JButton) tablero.getComponent(i);
+            t1.botones.add(b);
+        }
+
         ventana.add(tablero, BorderLayout.CENTER);
         ventana.add(puntaje, BorderLayout.SOUTH);
 
@@ -154,25 +169,8 @@ c=0
         b1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b1.getText() == ""){
-                    b1.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
-                System.out.println(winner);
+                t1.anotar(b1);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -185,23 +183,8 @@ c=0
         b2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b2.getText() == ""){
-                    b2.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b2);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -214,23 +197,8 @@ c=0
         b3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b3.getText() == ""){
-                    b3.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b3);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -243,23 +211,8 @@ c=0
         b4.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b4.getText() == ""){
-                    b4.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b4);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -272,23 +225,8 @@ c=0
         b5.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b5.getText() == ""){
-                    b5.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b5);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -301,23 +239,8 @@ c=0
         b6.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b6.getText() == ""){
-                    b6.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b6);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -330,23 +253,8 @@ c=0
         b7.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b7.getText() == ""){
-                    b7.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b7);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -359,23 +267,8 @@ c=0
         b8.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b8.getText() == ""){
-                    b8.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b8);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -388,23 +281,8 @@ c=0
         b9.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String t = "";
-                if(j){
-                    t = "X";
-                }
-                else{
-                    t = "O";
-                }
-                if(b9.getText() == ""){
-                    b9.setText(t);
-                    if(j){
-                        j = false;
-                    }
-                    else{
-                        j = true;
-                    }
-                }
-                winner = ver_ganador(tablero);
+                t1.anotar(b9);
+                winner = t1.ver_ganador(tablero);
                 if(winner == 1){
                     ganador.setText("Ganó el jugador 1");
                 }
@@ -415,13 +293,6 @@ c=0
         });
 
         ventana.setVisible(true);
- /*
-    c=0
-    0 - 0 - X - X - X - X - 0 - X - 0
-
-    | O | O | X |
-    | X | X | X |
-    | O | X | O |
-*/
     }
+    
 }
