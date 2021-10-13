0f01c93b54072921cb64e56a439cc536306271a9
brunocala7 <80712515+brunocala7@users.noreply.github.com>
2021-10-13 11:33:40 -0300

    Add files via upload

---
+++ Laboratorio/ejercicios/4.10/src/com/company/AccesoBaseDeDatos.java
@@ -0,0 +1,132 @@
+package com.company;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class AccesoBaseDeDatos {
+
+    private Connection conexion;
+    private String nombreBaseDeDatos;
+    private String nombreTabla;
+
+    public AccesoBaseDeDatos(String nombreBaseDeDatos, String nombreTabla) {
+        this.nombreBaseDeDatos = nombreBaseDeDatos;
+        this.nombreTabla = nombreTabla;
+    }
+
+    public void conectar(String user, String password) {
+
+        String url = "jdbc:mysql://localhost:3306/" + this.nombreBaseDeDatos;
+        /* línea alternativa para los que usan windows:
+        String url = "jdbc:mysql://localhost/" + this.nombreBaseDeDatos + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        */
+
+        try {
+
+            conexion = DriverManager.getConnection(url, user, password);
+
+            if (conexion != null) {
+                System.out.println("Se ha conectado exitosamente con la base de datos");
+            } else {
+                System.out.println("No se ha podido conectar con la base de datos");
+            }
+
+        } catch (SQLException excepcion) {
+            excepcion.printStackTrace();
+        }
+
+    }
+
+    public void modificarTabla(String consulta) {
+        /* INSERT, UPDATE, DELETE */
+        try {
+            Statement sentencia = this.conexion.createStatement();
+            sentencia.executeUpdate(consulta);
+            sentencia.close();
+
+        } catch (SQLException excepcion) {
+            excepcion.printStackTrace();
+        }
+    }
+
+    public ResultSet obtenerResultado(String consulta){
+
+        ResultSet resultado = null;
+
+        try {
+
+            Statement sentencia = this.conexion.createStatement();
+            resultado = sentencia.executeQuery(consulta);
+
+
+        } catch (SQLException excepcion) {
+            excepcion.printStackTrace();
+        }
+
+        return resultado;
+    }
+
+    public ResultSet seleccionarTodo(){
+        String consulta = "SELECT * FROM " + this.nombreTabla;
+        ResultSet resultado = this.obtenerResultado(consulta);
+        return resultado;
+    }
+
+    public void insertarDatos (Persona persona) {
+
+    }
+
+    public void imprimirDatos() {
+
+        ResultSet resultado = this.seleccionarTodo();
+
+        try {
+
+            while (resultado.next()) {
+
+                int id = resultado.getInt("id");
+                String nombre = resultado.getString("nombre");
+                String apellido = resultado.getString("apellido");
+                int edad = resultado.getInt("edad");
+
+                System.out.println(id + " " + nombre + " " + apellido + " " + edad);
+            }
+
+            resultado.close();
+
+        } catch (SQLException excepcion) {
+            excepcion.printStackTrace();
+        }
+    }
+
+    public ResultSet mostrarColumnas(){
+        String consulta = "SHOW COLUMNS FROM " + this.nombreTabla;
+        ResultSet resultado = this.obtenerResultado(consulta);
+        return resultado;
+    }
+
+    public ArrayList<String> colocarCamposEnLista() {
+
+        ArrayList<String> nombresDeCampos = new ArrayList<>();
+        ResultSet resultado = this.mostrarColumnas();
+
+        try {
+
+            while (resultado.next()) {
+                String nombreDeColumna = resultado.getString("field");
+                nombresDeCampos.add(nombreDeColumna);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return nombresDeCampos;
+    }
+
+    /*
+    Colocar mysql-connector-java-8.0.21.jar en una carpeta llamada lib
+    File -> Project Structure -> + -> JARs y directorios ->
+    seleccionar mysql-connector-java-8.0.21.jar -> tildar -> aplicar -> ok
+    */
+}
---
+++ Laboratorio/ejercicios/4.10/src/com/company/ManejoDeArchivos.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class ManejoDeArchivos {
+
+}
